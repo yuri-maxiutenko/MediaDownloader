b57c28e8cfae4951c3c69a3d8cfefd5fe96f6e11
Yuri Maxutenko <the.wolf.cub@gmail.com>
2020-03-11 00:38:43 +0200

    Added the basic UI.

---
+++ MainWindowView.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace YoutubeDownloader
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly MainWindowViewModel _model;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _model = new MainWindowViewModel();
+            DataContext = _model;
+        }
+
+        private void OnClearButtonClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ MainWindowViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System.Windows.Input;
+
+namespace YoutubeDownloader
+{
+    class MainWindowViewModel
+    {
+        private ICommand _clearButtonClick = null;
+        public ICommand ClearButtonClick
+        {
+            get
+            {
+                return _clearButtonClick ?? (_clearButtonClick = new RelayCommand(
+                    param => { },
+                    param => true));
+            }
+        }
+
+        private ICommand _browseButtonClick = null;
+        public ICommand BrowseButtonClick
+        {
+            get
+            {
+                return _browseButtonClick ?? (_browseButtonClick = new RelayCommand(
+                    param => { },
+                    param => true));
+            }
+        }
+
+        private ICommand _downloadButtonClick = null;
+        public ICommand DownloadButtonClick
+        {
+            get
+            {
+                return _downloadButtonClick ?? (_downloadButtonClick = new RelayCommand(
+                    param => { },
+                    param => true));
+            }
+        }
+    }
+}
