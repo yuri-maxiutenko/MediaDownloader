cfc4cd2751f4a65814b788b83413b9b093d707ef
Yuri Maxutenko <the.wolf.cub@gmail.com>
2020-04-11 17:29:23 +0300

    Revert "Revert "Added the saving of the download folders history to database.""
    This reverts commit ab75f3063228e3c4bd4d38d3d0e54ad0fdf25473.

---
+++ Database/Models/DownloadFolder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace YoutubeDownloader.Database.Models
+{
+    public class DownloadFolder
+    {
+        public int DownloadFolderId { get; set; }
+        public string Path { get; set; }
+        public DateTime LastSelectionDate { get; set; }
+
+        public override string ToString()
+        {
+            return Path;
+        }
+    }
+}
