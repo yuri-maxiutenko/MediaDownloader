9b7ac914abba814e48d94c7f74bf92a15d534ad6
Yuri Maxiutenko <the.wolf.cub@gmail.com>
2023-04-22 20:58:54 +0300

    feat: Update the solution to .NET 7
    Fixed Main Window opening
    
    github: https://github.com/yuri-maxiutenko/MediaDownloader/issues/2

--- MediaDownloader/App.xaml.cs
+++ MediaDownloader/App.xaml.cs
@@ -1,10 +1,8 @@
-﻿using System.Windows;
-
-namespace MediaDownloader;
+﻿namespace MediaDownloader;
 
 /// <summary>
 ///     Interaction logic for App.xaml
 /// </summary>
-public partial class App : Application
+public partial class App
 {
 }
