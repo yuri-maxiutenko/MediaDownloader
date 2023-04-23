f51ae96126201af2b938988893467aae0e9069c5
Yuri Maxiutenko <the.wolf.cub@gmail.com>
2023-04-23 19:04:13 +0300

    feat: Added humanizing Date/Time
    Added humanizing date/time
    
    github: https://github.com/yuri-maxiutenko/MediaDownloader/issues/11

---
+++ MediaDownloader/UI/Converters/HumanizedDateTimeConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+using Humanizer;
+
+namespace MediaDownloader.UI.Converters;
+
+[ValueConversion(typeof(DateTime), typeof(string))]
+public class HumanizedDateTimeConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        var dateTime = (DateTime?)value;
+        return dateTime?.ToUniversalTime().Humanize();
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotSupportedException("HumanizedDateTimeConverter: reverse-conversion not supported.");
+    }
+}
